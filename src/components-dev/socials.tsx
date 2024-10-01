59ca035f6e402f31891c941304f6c579a6a8467a
Azzeddine Boubekeur <azzizou@gmail.com>
2024-10-01 21:38:31 +0200

    icons to public

--- src/components-dev/socials.tsx
+++ src/components-dev/socials.tsx
@@ -2,10 +2,10 @@ export default function Socials() {
   return (
     <footer className="footer">
       <div className="socials">
-        <img className="social-ico" src="src/assets/github-ico.svg"></img>
-        <img className="social-ico" src="src/assets/fb-ico.svg"></img>
-        <img className="social-ico" src="src/assets/ig-ico.svg"></img>
-        <img className="social-ico" src="src/assets/x-ico.svg"></img>
+        <img className="social-ico" src="public/github-ico.svg"></img>
+        <img className="social-ico" src="public/fb-ico.svg"></img>
+        <img className="social-ico" src="public/ig-ico.svg"></img>
+        <img className="social-ico" src="public/x-ico.svg"></img>
       </div>
     </footer>
   );
