fc267c835b80084bc0490bff4fbb67787b984f3c
Azzeddine Boubekeur <azzizou@gmail.com>
2024-10-01 21:43:08 +0200

    changing the url for the icons

--- src/components-dev/socials.tsx
+++ src/components-dev/socials.tsx
@@ -2,10 +2,10 @@ export default function Socials() {
   return (
     <footer className="footer">
       <div className="socials">
-        <img className="social-ico" src="public/github-ico.svg"></img>
-        <img className="social-ico" src="public/fb-ico.svg"></img>
-        <img className="social-ico" src="public/ig-ico.svg"></img>
-        <img className="social-ico" src="public/x-ico.svg"></img>
+        <img className="social-ico" src="github-ico.svg"></img>
+        <img className="social-ico" src="fb-ico.svg"></img>
+        <img className="social-ico" src="ig-ico.svg"></img>
+        <img className="social-ico" src="x-ico.svg"></img>
       </div>
     </footer>
   );
