3d55d36ca867091301cf353582b878ef44e15d1e
Azzeddine Boubekeur <azzizou@gmail.com>
2024-10-04 22:51:47 +0200

    ui update

--- src/components-dev/tools-used.tsx
+++ src/components-dev/tools-used.tsx
@@ -1,8 +1,7 @@
 export default function ToolsUsed(props: any) {
   return (
     <div className="tool-used" key={"props.tools"}>
-      {" "}
-      {props.tools}
+      <h6> {props.tools}</h6>
     </div>
   );
 }
